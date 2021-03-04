fdadf8ec008821d66a0693a9756b317d6d525ae8
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-04 15:29:21 +0500

    Add helper methods and Associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,14 +1,4 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend, :class_name => "User"
-
-  # Users who have yet to confirme friend requests
-  def pending_friends
-    friendships.map{|friendship| friendship.friend_id if !friendship.status}.compact
-  end
-
-  # Users who have requested to be friends
-  def friend_requests
-    inverse_friendships.map{|friendship| friendship.user_id if !friendship.status}.compact
-  end
+  belongs_to :friend, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,33 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
+
   has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend_id if friendship.status}
-    friends_array + inverse_friendships.map{|friendship| friendship.user_id if friendship.status}
+    friends_array = friendships.map { |friendship| friendship.friend_id if friendship.status }
+    friends_array += inverse_friendships.map { |friendship| friendship.user_id if friendship.status }
     friends_array.compact
   end
-  
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map { |friendship| friendship.friend_id unless friendship.status }.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user_id if !friendship.status }.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find { |fship| fship.user_id == user }
+    friendship.status = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
