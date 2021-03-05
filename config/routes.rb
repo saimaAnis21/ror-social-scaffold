8722bbca42fac82a1c5dd1223a9661b3640d576f
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-05 17:21:16 +0500

    Add friendship routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show]
+  resources :friendships, only: [:new, :update, :destroy]
+  get 'friendships/(:id)', to: 'friendships#update'
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
