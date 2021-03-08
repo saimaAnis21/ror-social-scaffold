7f506f9ad99c04e84886d063c947f3b5a5d98913
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-08 15:46:11 +0500

    Remove rubocop error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    @friend_list = User.find((@user.friends))
+    @friend_list = User.find(@user.friends)
     @req_rec = User.find(@user.friend_requests)
     @req_send = User.find(@user.pending_friends)
   end
