1ab5810eba14b6bcdc8172b9d3471573c19a49f3
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-09 19:34:49 +0500

    Add rspec for comment_spec

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -19,16 +19,20 @@ RSpec.feature 'Users' do
       @user = User.create(name: 'user1', email: 'user1@mail.com', password: '123456')
       @user = User.create(name: 'user2', email: 'user2@mail.com', password: '123456')
       @post = Post.create(user_id:1, content:'aaaaaa')
+      @frnd = Friendship.create(user_id:1,friend_id:2, status: 'true')
     end
   
-    scenario 'user1 writes a comment' do
+    scenario 'user2 writes a comment' do
+      
       visit root_path
-      fill_in 'Email', with: 'user1@mail.com'
+      fill_in 'Email', with: 'user2@mail.com'
       fill_in 'Password', with: '123456'
       click_on 'Log in'
       click_on 'All users'
-      find('link', text: 'See Profile').click_on
-      fill_in 'comment', with: 'This is a test comment!'
-      sleep(5)
+      page.find_by_id(1).click
+      fill_in 'comment_content', with: 'This is a test comment!'
+      click_on 'Comment'
+      expect(page).to have_content 'This is a test comment!'
+      sleep(10)
     end
   end
