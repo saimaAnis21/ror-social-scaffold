cf820f493969d171a3e3cc4f1e64ad1e9238c7d7
necmigunduz <necmigunduz@gmail.com>
2021-03-10 16:19:02 +0300

    Fix post rspec test

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -7,7 +7,8 @@ end
 
 RSpec.describe Post, type: :model do
   it 'is valid with a content and a user_id' do
-    post = Post.create(user_id: 1, content: 'aaaaaa')
+    user = User.create(name:'Sam', email:'sam@sample.com', password:'123456')
+    post = Post.create(user_id: user.id, content: 'aaaaaa')
     expect(post).to be_valid
   end
 end
